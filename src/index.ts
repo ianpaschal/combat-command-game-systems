dc2a6fd3e31a646112e4621973303cd9a88e9829
Ian Paschal <ianpaschal@users.noreply.github.com>
2025-10-14 21:00:55 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as FlamesOfWarV4 from './battlefront/flamesOfWarV4';
+export * as TeamYankeeV2 from './battlefront/teamYankeeV2';
 export * as Common from './common';
 export * as Utils from './common/_internal';
