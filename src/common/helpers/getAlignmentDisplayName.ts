5c49bc22d6218b8815d30697255475255c12c35f
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-01-24 21:17:36 +0100

    Update getAlignmentDisplayName.ts

--- src/common/helpers/getAlignmentDisplayName.ts
+++ src/common/helpers/getAlignmentDisplayName.ts
@@ -2,12 +2,11 @@ import { alignments as flamesOfWarV4 } from '../../battlefront/flamesOfWarV4/sta
 import { alignments as teamYankeeV2 } from '../../battlefront/teamYankeeV2/static/alignments';
 import { GenericMetadata } from '../types';
 
+const mergedAlignments: Record<string, GenericMetadata> = {
+  ...flamesOfWarV4,
+  ...teamYankeeV2,
+};
+
 export const getAlignmentDisplayName = (
   key: string,
-): string | undefined => {
-  const alignments: Record<string, GenericMetadata> = {
-    ...flamesOfWarV4,
-    ...teamYankeeV2,
-  };
-  return alignments[key]?.displayName;
-};
+): string | undefined => mergedAlignments[key]?.displayName;
