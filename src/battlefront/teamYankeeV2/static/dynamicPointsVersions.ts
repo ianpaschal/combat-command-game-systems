1085de5d486fdb1ccc351bd1de6476df2005bef5
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-01-28 13:50:12 +0100

    Add Checkpoint Charlie points version

--- src/battlefront/teamYankeeV2/static/dynamicPointsVersions.ts
+++ src/battlefront/teamYankeeV2/static/dynamicPointsVersions.ts
@@ -4,12 +4,19 @@ import { DynamicPointsVersionMetadata } from '../../_shared/types';
 import { Era } from './eras';
 
 export enum DynamicPointsVersion {
-  Original = 'original',
-  Dynamic2024 = '2024_07',
-  Dynamic2025 = '2025_01',
+  CCOriginal = 'cc_original',
+  Original = 'original', // FIXME: Rename to wwiii_original, required DB migration
+  Dynamic2024 = '2024_07', // FIXME: Rename to wwiii_dynamic_2024_07, required DB migration
+  Dynamic2025 = '2025_01', // FIXME: Rename to wwiii_dynamic_2025_01, required DB migration
 }
 
 export const dynamicPointsVersions: Record<DynamicPointsVersion, DynamicPointsVersionMetadata<Era>> = {
+  [DynamicPointsVersion.CCOriginal]: {
+    displayName: 'Original Book',
+    publishedAt: '2015-01-01T00:00:00+00:00',
+    activeAt: '2015-01-01T00:00:00+00:00',
+    era: Era.Early,
+  },
   [DynamicPointsVersion.Original]: {
     displayName: 'Original Book',
     publishedAt: '2015-01-01T00:00:00+00:00',
