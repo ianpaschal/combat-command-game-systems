7a84c4237951fda73cd17bf5dcf83b50a435edfc
Ian Paschal <ianpaschal@users.noreply.github.com>
2026-06-05 10:47:55 +0200

    Export Great War

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as FlamesOfWarV4 from './battlefront/flamesOfWarV4';
+export * as GreatWarV4 from './battlefront/greatWarV4';
 export * as TeamYankeeV2 from './battlefront/teamYankeeV2';
 export * as Common from './common';
 export * as Utils from './common/_internal';
